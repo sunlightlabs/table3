7af40f8f4a94159ce7559f5481160d9d740c5992
Alex Engler <aengler@sunlightfoundation.com>
2012-06-15 16:11:04 -0400

    This is an update to the Table3 Parser: Now with parsing.

--- Table3_Parser2.py
+++ Table3_Parser2.py
@@ -4,10 +4,10 @@
 #Sunlight Foundation
 
 
-import sys
 import re
 from collections import OrderedDict
 import csv
+import copy
 
 EXPRESSIONS = {
 	'usc_title': '^\d\d?A?',
@@ -17,10 +17,6 @@ EXPRESSIONS = {
 	'statutes_at_large': '\s\d*[,-]?[ ]?\d*$',    
 	'description': '(nt[\[\]a-zA-Z ]*)|(ed chg)|(prec[a-zA-Z ]*)|(new)|(gen amd)|(omitted)|(repealed)|(tr to [\w/-]*)|(tr fr [\w/-]*)|([a-zA-Z -]+)',
 }
-
-#account for any weird descriptions
-
-#account for additional sections that should be separate entries
 	
 LINE_TYPES = {
 	'normal_line': re.compile(r"{usc_title} [ ]* {usc_section}[ ]* ({description})?[ ]* {public_law_number}[ ]* ({public_law_section})?[ ]* {statutes_at_large}$".format(**EXPRESSIONS)),
@@ -38,34 +34,29 @@ def parse_table3(file):
 	lines = re.compile(r'\n').split(full_content)  
 	del full_content
 	
-	current_usc_title = None
-	current_usc_section = None
-	current_public_law_number = None
-	current_public_law_section = None
-	current_statutes_at_large = None
-	parsed = OrderedDict()
+	usc_title = None
+	usc_section = None
+	public_law_number = None
+	public_law_section = None
+	statutes_at_large = None
 	
+	line_data = {}		
+
 	for line in lines:
 		if not line.strip():
 			continue
-
+			
 		classified = classify(line)
-		line_data = None
+		previous_line_data = copy.deepcopy(line_data)
 		
 		if classified['normal_line']:
 			
-			f = re.match("(?P<current_usc_title>{usc_title})[ ]+(?P<current_usc_section>{usc_section})[ ]+(?P<current_description>{description})[ ]+(?P<current_public_law_number>{public_law_number})[ ]+(?P<current_public_law_section>{public_law_section})[ ]+(?P<current_statutes_at_large>{statutes_at_large})".format(**EXPRESSIONS), line)
+			f = re.match("(?P<usc_title>{usc_title})[ ]+(?P<usc_section>{usc_section})[ ]+(?P<description>{description})[ ]+(?P<public_law_number>{public_law_number})[ ]+(?P<public_law_section>{public_law_section})[ ]+(?P<statutes_at_large>{statutes_at_large})".format(**EXPRESSIONS), line)
 
 			line_data = f.groupdict()
-
-			current_usc_title = line_data['current_usc_title']
-			current_usc_section = line_data['current_usc_section']
-			current_description = line_data['current_description']
-			current_public_law_number = line_data['current_public_law_number']
-			current_public_law_section = line_data['current_public_law_section']
-			current_statutes_at_large = line_data['current_statutes_at_large']
-			
-			##something like  writer = csv.writer(open('table.csv', 'wb')?
+			line_data = {k :v.strip() for k, v in line_data.iteritems()} #removes whitespace			
+		
+			writer.writerow(line_data)
 			
 			print line_data
 		
@@ -74,26 +65,31 @@ def parse_table3(file):
 			f = re.match("(?P<additional_description>.*$)", line)
 			
 			line_data = f.groupdict()
-		
-			current_additional_description = line_data['additional_description']
-			
-			print line_data			
-		
+			line_data = {k :v.strip() for k, v in line_data.iteritems()} #removes whitespace		
+						
+			print line_data
        
 		if not any(classified):
-			print 'Something went wrong...', line
-			print parsed
+			print 'What kind of line is this?', line
 			sys.exit(1)
 
 
-
-#Open file
 file_target = raw_input("Enter Table 3 file to be parsed: ")
 file = open(file_target)	
-#/Users/AlexEngler/Desktop/Python Docs/Table3_112.txt
-	
+
+output_file = open('table3.csv', 'w')
+
+writer= csv.DictWriter(output_file, ['usc_title', 'usc_section', 'public_law_number', 'public_law_section', 'statutes_at_large', 'description'])
+writer.writeheader()
+
 parse_table3(file)
 
-#export to .csv
+output_file.close()
 
 
+#TO DO:
+#create separate rows for multiple usc_section listings
+#add additional descriptions to previous dict
+#name of csv
+#scrapper compliance
+#statues being picked up as public law sections when there is no public law section
